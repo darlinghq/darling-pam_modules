87d0f510d9a0540a7895fa311ef0ddb1e32cf81f
Thomas A <curioustommy@protonmail.com>
2022-11-20 12:26:54 -0800

    Fix int-conversion error

--- modules/pam_tid/pam_tid.c
+++ modules/pam_tid/pam_tid.c
@@ -33,6 +33,10 @@
 #include <pwd.h>
 #include <LocalAuthentication/LAPrivateDefines.h>
 
+#ifdef DARLING
+#include <LocalAuthentication/LACFSupport.h>
+#endif
+
 #define PAM_SM_AUTH
 #define PAM_SM_ACCOUNT
 
